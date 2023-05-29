8adc79aaac6a5f5e2b934948c0203324dacd7a4d
Taylor Monacelli <taylormonacelli@gmail.com>
2023-05-29 00:17:25 -0700

    ok

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/taylormonacelli/bootedseeds
 
 go 1.20
+
+require github.com/aws/aws-sdk-go v1.44.271
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- yourpackage/yourcode_test.go
+++ yourpackage/yourcode_test.go
@@ -1,15 +1,13 @@
-package yourpackage
+package yourpackage_test
 
 import (
 	"fmt"
 	"path/filepath"
 	"testing"
-
-	"github.com/taylormonacelli/bootedseeds/testdata"
 )
 
 func TestYourFunction(t *testing.T) {
-	dataPath := filepath.Join(testdata.Dir(), "data.json")
+	dataPath := filepath.Join("testdata", "data.json")
 
 	fmt.Println(dataPath)
 	// Use dataPath in your test function
